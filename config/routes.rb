81f1b46ad660a0deba91a192c4c477a9a8488c15
gildas fremont <gildas.fremont@gmail.com>
2016-08-08 12:28:02 +0200

    sans css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'answer', to: 'coaching#answer'
+
+  get 'coaching/ask', to: 'coaching#ask'
+
+  get 'coaching_controller/answer'
+
+  get 'coaching_controller/ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
